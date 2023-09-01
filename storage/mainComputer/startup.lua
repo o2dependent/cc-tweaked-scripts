ccad50624cd630598bdfa804d55f8c6100803b8b
Ethan Olsen <131eolsen@gmail.com>
2023-09-01 05:35:35 -0700

    fix: local install scripts

--- storage/mainComputer/startup.lua
+++ storage/mainComputer/startup.lua
@@ -1,7 +1,6 @@
 -- transmit channel 43 - turtle
 -- receive channel 1 - main computer
 
-print('packaged installed via github')
 print('packaged loaded')
 local modem = peripheral.find("modem") or error("No modem attached", 0)
 modem.open(43) -- Open 43 so we can receive replies
@@ -9,19 +8,24 @@ modem.open(43) -- Open 43 so we can receive replies
 -- Send our message
 modem.transmit(15, 43, "Hello, world!")
 
-
-
-while (true) do
+function modemMessageHandler()
 	-- pull event from modem and process via channel
 	local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
 
 	if (channel == 1) then
 		print("Received a message: " .. tostring(message))
 	end
+	return true
+end
 
+function clickHandler()
 	event, button, x, y = os.pullEvent("mouse_click")
 	print(("The mouse button %s was pressed at %d, %d"):format(button, x, y))
-
-
+	return true
 end
+
+repeat
+	clickHandler()
+	modemMessageHandler()
+until false
 -- And wait for a reply
