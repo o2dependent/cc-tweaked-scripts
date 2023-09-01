a45188f98ee68eef1375c003815a659c164950bb
Ethan Olsen <131eolsen@gmail.com>
2023-09-01 06:31:30 -0700

    feat: communicating between devices

--- storage/mainComputer/startup.lua
+++ storage/mainComputer/startup.lua
@@ -2,30 +2,39 @@
 -- receive channel 1 - main computer
 
 print('packaged loaded')
-local modem = peripheral.find("modem") or error("No modem attached", 0)
-modem.open(43) -- Open 43 so we can receive replies
+modem = peripheral.find("modem") or error("No modem attached", 0)
+modem.open(1) -- Open 43 so we can receive replies
 
 -- Send our message
 modem.transmit(15, 43, "Hello, world!")
 
-function modemMessageHandler()
+--- --- HANDLERS --- ---
+-- modem handler
+function modemMessageHandler(data)
 	-- pull event from modem and process via channel
-	local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-
+	local event, side, channel, replyChannel, message, distance = table.unpack(data)
+	print(channel, message)
 	if (channel == 1) then
 		print("Received a message: " .. tostring(message))
+		io.open("data.txt", "w"):write(message):close()
 	end
-	return true
 end
 
-function clickHandler()
-	event, button, x, y = os.pullEvent("mouse_click")
+-- click handler
+function clickHandler(data)
+	local event, button, x, y = table.unpack(data)
 	print(("The mouse button %s was pressed at %d, %d"):format(button, x, y))
-	return true
+	modem.transmit(43, 1, "chest_data")
 end
 
-repeat
-	clickHandler()
-	modemMessageHandler()
-until false
--- And wait for a reply
+--- --- MAIN --- ---
+while true do
+	local eventData = {os.pullEvent()}
+	local event = eventData[1]
+
+	if event == "mouse_click" then
+		clickHandler(eventData)
+	elseif event == "modem_message" then
+		modemMessageHandler(eventData)
+	end
+end
--- storage/turtle/startup.lua
+++ storage/turtle/startup.lua
@@ -1,14 +1,42 @@
 print('packaged installed via github')
 print('packaged loaded')
-local modem = peripheral.find("modem") or error("No modem attached", 0)
+modem = peripheral.find("modem") or error("No modem attached", 0)
 modem.open(43) -- Open 43 so we can receive replies
 
 -- Send our message
-modem.transmit(15, 43, "Hello, world!")
+modem.transmit( 43,1, "Hello, world!")
 
--- And wait for a reply
-local event, side, channel, replyChannel, message, distance
-repeat  event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-until channel == 43
+--- --- FUNCTIONS --- ---
+function sendChestData()
+	print("Pulling chest data")
+	local chestData = {}
+	local chest = peripheral.find("minecraft:chest")
+	print(chest)
+	local chestItems = chest.list()
+	for i, v in pairs(chestItems) do
+		table.insert(chestData, v)
+	end
+	print("Sending chest data")
+	local chestDataString = textutils.serialize(chestData)
+	modem.transmit(1, 43, chestDataString)
+end
 
-print("Received a reply: " .. tostring(message))
+--- --- HANDLERS --- ---
+-- handle modem messages
+function modemMessageHandler(data)
+	local event, side, channel, replyChannel, message, distance = table.unpack(data)
+
+	if (channel == 43) then
+		sendChestData()
+	end
+end
+
+--- --- MAIN --- ---
+while true do
+	local eventData = {os.pullEvent()}
+	local event = eventData[1]
+
+	if event == "modem_message" then
+		modemMessageHandler(eventData)
+	end
+end
