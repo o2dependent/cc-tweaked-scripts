3568eb2a5ecf29729ac3deec78a995bd5cfdd1d8
Ethan Olsen <131eolsen@gmail.com>
2023-09-01 04:54:00 -0700

    chore: add mouse click test

--- storage/mainComputer/startup.lua
+++ storage/mainComputer/startup.lua
@@ -1,3 +1,6 @@
+-- transmit channel 43 - turtle
+-- receive channel 1 - main computer
+
 print('packaged installed via github')
 print('packaged loaded')
 local modem = peripheral.find("modem") or error("No modem attached", 0)
@@ -6,9 +9,21 @@ modem.open(43) -- Open 43 so we can receive replies
 -- Send our message
 modem.transmit(15, 43, "Hello, world!")
 
--- And wait for a reply
-local event, side, channel, replyChannel, message, distance
-repeat  event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-until channel == 43
 
-print("Received a reply: " .. tostring(message))
+
+while (true) do
+	-- pull event from modem and process via channel
+	local event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+
+	if (channel == 1) then
+		print("Received a message: " .. tostring(message))
+	end
+
+	event, button, x, y = os.pullEvent("mouse_click")
+	if (button == 1) then
+  	print(("The mouse button %s was pressed at %d, %d"):format(button, x, y))
+	end
+
+
+end
+-- And wait for a reply
